869acfc564aecaf4d33dad942fc2cc996d9db98b
mohitverma4109 <mohitverma4109@gmail.com>
2024-10-13 19:48:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 region = "ap-south-1"
 }
 resource "aws_instance" "one" {
-  ami           = "ami-04823729c75214919"
+  ami           = "ami-078264b8ba71bc45e"
   instance_type = "t2.micro"
   key_name      = "Mohit"
   vpc_security_group_ids = ["sg-0be48c52ba8131e2e"]
