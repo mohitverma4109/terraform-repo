f90f0c83511c4a700710b8731cd4f6f833c3569e
mohitverma4109 <mohitverma4109@gmail.com>
2024-10-24 11:34:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,15 @@ provider "aws" {
 region = "ap-south-1"
 }
 resource "aws_instance" "one" {
-  ami           = "ami-078264b8ba71bc45e"
-  instance_type = "t2.micro"
+  count         = 4
+  ami           = "ami-04a37924ffe27da53"
+  instance_type = "t2.medium"
   key_name      = "Mohit"
   vpc_security_group_ids = ["sg-0be48c52ba8131e2e"]
   tags = {
-    Name = "server-2"
+    Name = var.instance_name[count.index]
   }
+}
+variable "instance_name" {
+default = ["tomcat", "nexus", "jenkins", "slave"]
 }
